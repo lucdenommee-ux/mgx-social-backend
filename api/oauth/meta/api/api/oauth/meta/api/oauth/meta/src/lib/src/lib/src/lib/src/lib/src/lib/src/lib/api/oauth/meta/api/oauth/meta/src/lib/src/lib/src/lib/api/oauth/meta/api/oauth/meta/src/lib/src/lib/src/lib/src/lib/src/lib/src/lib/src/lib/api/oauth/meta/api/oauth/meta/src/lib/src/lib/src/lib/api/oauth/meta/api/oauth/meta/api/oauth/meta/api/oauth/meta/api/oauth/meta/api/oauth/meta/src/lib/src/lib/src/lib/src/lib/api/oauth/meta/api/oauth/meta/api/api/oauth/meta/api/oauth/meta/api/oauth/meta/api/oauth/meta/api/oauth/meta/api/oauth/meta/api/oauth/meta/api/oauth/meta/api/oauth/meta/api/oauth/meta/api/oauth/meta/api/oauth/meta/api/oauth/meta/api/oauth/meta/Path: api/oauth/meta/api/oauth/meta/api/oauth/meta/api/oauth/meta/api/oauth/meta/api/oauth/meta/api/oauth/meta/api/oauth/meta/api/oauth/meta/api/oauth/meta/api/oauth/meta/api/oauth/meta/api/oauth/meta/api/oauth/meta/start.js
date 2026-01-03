c5c2be11433f29df85c247f1b1b00e6b5c1212f9
lucdenommee-ux <lucdenommee@gmail.com>
2026-01-03 23:25:59 +0800

    Create start.js

---
+++ api/oauth/meta/api/api/oauth/meta/api/oauth/meta/src/lib/src/lib/src/lib/src/lib/src/lib/src/lib/api/oauth/meta/api/oauth/meta/src/lib/src/lib/src/lib/api/oauth/meta/api/oauth/meta/src/lib/src/lib/src/lib/src/lib/src/lib/src/lib/src/lib/api/oauth/meta/api/oauth/meta/src/lib/src/lib/src/lib/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/src/lib/src/lib/src/lib/src/lib/api/oauth/meta/api/oauth/meta/api/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/Path: api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/api/oauth/meta/start.js
@@ -0,0 +1,12 @@
+const { getMetaAuthUrl } = require("../../../src/lib/metaAuth");
+const { jsonError } = require("../../../src/lib/respond");
+module.exports = (req, res) => {
+  try {
+    const url = getMetaAuthUrl();
+    res.statusCode = 302;
+    res.setHeader("Location", url);
+    res.end();
+  } catch (e) {
+    jsonError(res, 500, e.message || "Failed to build Meta auth url");
+  }
+};
